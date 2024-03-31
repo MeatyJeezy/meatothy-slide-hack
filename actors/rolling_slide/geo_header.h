e3f6520a4e7754dde77569574de42cd5a074d019
root <root@DESKTOP-NS4RMGN.localdomain>
2024-03-31 02:46:43 -0600

    lots of level 2 edits, rolling slide model replacement, level 1 tweaks

--- actors/rolling_slide/geo_header.h
+++ actors/rolling_slide/geo_header.h
@@ -1,6 +1,8 @@
 extern const GeoLayout rolling_slide_geo[];
-extern Lights1 rolling_slide_f3dlite_material_003_lights;
-extern Vtx rolling_slide_Cube_003_mesh_layer_1_vtx_0[24];
-extern Gfx rolling_slide_Cube_003_mesh_layer_1_tri_0[];
-extern Gfx mat_rolling_slide_f3dlite_material_003[];
-extern Gfx rolling_slide_Cube_003_mesh_layer_1[];
+extern u8 rolling_slide_stone2_ci8[];
+extern u8 rolling_slide_stone2_pal_rgba16[];
+extern Vtx rolling_slide_Cube_003_mesh_layer_4_vtx_0[24];
+extern Gfx rolling_slide_Cube_003_mesh_layer_4_tri_0[];
+extern Gfx mat_rolling_slide_Stone2[];
+extern Gfx mat_revert_rolling_slide_Stone2[];
+extern Gfx rolling_slide_Cube_003_mesh_layer_4[];
