aa2fa9ea73d9946ae756291cbf4c3ed23923cdf0
root <root@DESKTOP-NS4RMGN.localdomain>
2024-03-31 22:43:48 -0600

    level 2 fixes and end door

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -286,8 +286,8 @@ and don't come back!\n\
 Gwa ha ha!"))
 
 DEFINE_DIALOG(DIALOG_022, 1, 2, 95, 200, _("\
-You need a key to open\n\
-this door."))
+You need 4 stars to\n\
+open this door."))
 
 DEFINE_DIALOG(DIALOG_023, 1, 3, 95, 200, _("\
 This key doesn't fit!\n\
